634a18056f2b05675335d72c29dfad157f3bd8a0
bella77-69 <mrspasceri@hotmail.com>
2024-03-30 14:00:14 -0700

    added passport, mysql, set up server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,36 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-require("dotenv").config();
-const PORT = process.env.PORT || 8080;
+import express from "express";
+import session from "express-session";
+import passport from "./middleware/passport.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 8080;
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: "secret",
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: false, // HTTPS Required
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+import authRoute from "./routers/authRoute.js";
+import indexRoute from "./routers/indexRoute.js";
+
+app.use('/', indexRoute);
+app.use("/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
