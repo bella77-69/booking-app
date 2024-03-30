d1f78991f41e0efd35e46fc238922649928a3b6a
bella77-69 <mrspasceri@hotmail.com>
2024-03-30 07:36:07 -0700

    installed dependencies for the server

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-PORT = process.env.PORT;
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port: ${process.env.PORT}`);
-    });
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
