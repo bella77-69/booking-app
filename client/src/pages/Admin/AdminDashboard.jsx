4e5639d371b777a9c0ab9aaa5d7c8bf1a54074dd
bella77-69 <mrspasceri@hotmail.com>
2024-08-18 19:26:00 -0700

    removed unwanted code

--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -10,7 +10,7 @@ import {
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import "./calendarStyles.css"; // Import your custom CSS
+import "./calendarStyles.css"; 
 
 const AdminDashboard = () => {
   const [appointments, setAppointments] = useState([]);
