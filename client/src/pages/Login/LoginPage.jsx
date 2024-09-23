c311ab155686b3846ae36589829bbfa9eb43011f
bella77-69 <mrspasceri@hotmail.com>
2024-09-22 19:10:21 -0700

    updated vite.config.js file, set up for client-side routing

--- client/src/pages/Login/LoginPage.jsx
+++ client/src/pages/Login/LoginPage.jsx
@@ -30,7 +30,8 @@ function Login() {
     }));
 
     try {
-      const res = await axios.post(`http://localhost:8000/api/user/login`, {
+      // const res = await axios.post(`http://localhost:8000/api/user/login`, {
+        const res = await axios.post(`https://booking-app-server-kx79.onrender.com/api/user/login`, {
         email,
         password,
       });
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This is important for client-side routing to work correctly in production.
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Enables history API fallback (dev mode)
+  },
 })
